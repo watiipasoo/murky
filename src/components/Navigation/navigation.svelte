b862404c16ed19be0248fc075aa21d60623b28e0
Watiipasoo <watipaso.chirambo@nicotechnologies.com>
2023-11-27 21:12:32 +0200

    changes

--- src/components/Navigation/navigation.svelte
+++ src/components/Navigation/navigation.svelte
@@ -1,6 +1,6 @@
 <nav class="w-[80%] py-8 mx-auto flex justify-between relative">
     <h4 class="font-bold text-lg">Digital Agency</h4>
-    <img src="/menu.png" alt="" class="h-[18px]"/>
+    <img src="/menu.png" alt="" class="h-[18px] lg:hidden"/>
     <ul class="hidden lg:flex gap-8">
         <li>
             home
